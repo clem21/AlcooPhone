df39aa69ec0369d0d6a609e6748b72a749d61eca
clem21 <clemence.pereira@hotmail.fr>
2018-02-27 22:07:38 +0100

    Page list drink + embrouille avec Xavier + echec de l'installation de la barrette de RAM

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -19,8 +19,11 @@ import { Http } from '@angular/http';
 export class ListPage {
 
   public xmlItems : any;
+  public categ : string;
+  public tab : Array<string>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.categ = this.navParams.get('categ');
   }
 
   ionViewWillEnter()
@@ -44,33 +47,51 @@ export class ListPage {
 
   parseXML(data)
   {
+    var that = this;
      return new Promise(resolve =>
      {
+       var listPage = ListPage;
         var k,
+            clem,
+            sous_clem,
             arr    = [],
             parser = new xml2js.Parser(
             {
                trim: true,
                explicitArray: true
             });
-
         parser.parseString(data, function (err, result)
         {
-           console.log(data);
            var obj = result;
            for(k in obj)
            {
+              //i++;
               var item = obj[k];
-              arr.push({
-                 name   : item.name,
-                 degre  : item.degre          
-              });
               console.log(item);
+              for(clem of item.category)
+              {
+                if(clem.name[0] == that.categ) {
+                  for(sous_clem of clem.item) {
+                    arr.push({
+                      id     : sous_clem.$.id,
+                      name   : sous_clem.name[0],
+                      degre  : sous_clem.degre[0]
+                    });
+                  }
+                }
+              }   
            }
-
-           resolve(obj);
+           resolve(arr);
         });
      });
+  }
+
+  remove(item_id){
+    this.tab = [];
+  }
+
+  add(item_id){
+
   }
 
   ionViewDidLoad() {
